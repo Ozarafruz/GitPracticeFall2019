9a9029cf48fb3afed4a8d49b0dad9953fa20c68f
ozarafruz <ozarafruz@gmail.com>
2020-03-11 20:14:10 -0400

    Modified array of colors

--- src/day2/Colors.java
+++ src/day2/Colors.java
@@ -3,8 +3,13 @@ package day2;
 public class Colors {
 
     public static void main(String[] args) {
-        String[] colors = {"red","blue","green"};
+        String[] colors = {"red","blue","green","yellow"};
 
+/*
+git status
+git add .
+git commit -m "message"
+ */
 
     }
 }
